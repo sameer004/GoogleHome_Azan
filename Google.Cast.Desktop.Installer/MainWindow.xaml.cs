6115fa76e8bb5afb019b8c7d7fa7536c9d5340e7
Saqib Khan <saqi19@hotmail.com>
2019-12-06 20:22:30 -0500

    restored

--- Google.Cast.Desktop.Installer/MainWindow.xaml.cs
+++ Google.Cast.Desktop.Installer/MainWindow.xaml.cs
@@ -24,32 +24,40 @@ namespace Google.Cast.Desktop.Installer
         {
             InitializeComponent();
         }
-
+        private string playerSelect { get; set; }
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             var a = new PrayerSetup<Azan>().SetUp(string.Format("https://muslimsalat.com/{0}/daily/{1}/false.json", "newyork", DateTime.Now.ToString("dd-MM-yyyy"))
                 , "0 1 0 1/1 * ? *"
-                , "Office Ustairs speaker");
+                , playerSelect);
         }
 
         private void cmb_SelectedChanged(object sender, SelectionChangedEventArgs e)
         {
-
+            btnSchedule.IsEnabled = true;
+            playerSelect = cmbCaster.SelectedValue as string;
         }
 
-        private void cmbCaster_Loaded(object sender, RoutedEventArgs e)
+        private async void cmbCaster_Loaded(object sender, RoutedEventArgs e)
         {
-
+            var p = new AzanPlayer();
+            var players = await p.GoogleCastPlayers();
+            cmbCaster.ItemsSource = players;
         }
 
         private void MainWindow_Closed(object sender, EventArgs e)
         {
-
+            System.Windows.Application.Current.Shutdown();
         }
 
         public static void UpdateStatus(string status)
         {
             
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            btnSchedule.IsEnabled = false;
         }
     }
 }
